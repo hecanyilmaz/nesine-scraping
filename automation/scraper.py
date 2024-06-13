df1cbd3ff8d7aa2e1aea1736ee745f20a88a82bc
hecanyilmaz <hcanylmzbotpre@gmail.com>
2024-06-13 14:15:29 +0300

    did some revisiting in the main part of the scraper

--- automation/scraper.py
+++ automation/scraper.py
@@ -5,19 +5,17 @@ from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.common.by import By
 import time
 
-class NesineBasketballDriver:
+class NesineScrapper:
 
     # Constants
     SYNC_SECOND = 2
-    driver = webdriver.Chrome()
-    
+
     def __init__(self):
+        self.driver = webdriver.Chrome()
         pass
 
     # Skipping insturction view
     def pass_entrance(self, sync_second=SYNC_SECOND):
-
-        # Waiting for to view skip instruction button
 
         # Skips instructions
         button_instructions = self.find_element_until(By.XPATH, '//*[@id="tutorial"]/div/div[1]/div[1]/button', wait=sync_second)
@@ -29,14 +27,22 @@ class NesineBasketballDriver:
         button_cookies = self.find_element_until(By.XPATH, '//*[@id="c-p-bn"]', wait=sync_second)
         button_cookies.click()
 
-    # Tries to find element between a time interval
+        time.sleep(1)    
+
+        # Closes summer advertisement and awaits for SYNC_SECOND
+        button_cookies = self.find_element_until(By.XPATH, '//*[@id="mod-base"]/div/div/div[1]/a', wait=sync_second)
+        button_cookies.click()
+
+        time.sleep(1)   
+
+    # Tries to find ELEMENT between a time interval
     def find_element_until(self, strategy, locator, wait=0, poll_frequency=0):
         try: 
             return WebDriverWait(self.driver, wait, poll_frequency=poll_frequency).until(lambda x: x.find_element(strategy, locator))
         except TimeoutException:
             raise NoSuchElementException
     
-    # Tries to find elements between a time interval
+    # Tries to find ELEMENTS between a time interval
     def find_elements_until(self, strategy, locator, wait=0, poll_frequency=0):
         try: 
             return WebDriverWait(self.driver, wait, poll_frequency=poll_frequency).until(lambda x: x.find_elements(strategy, locator))
